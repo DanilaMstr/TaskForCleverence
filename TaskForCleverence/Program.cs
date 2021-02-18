94da397f3a55df9d22da5c7c324d2fe52b6a66e2
Данила Понукаев <danila_ponukaev@mail.ru>
2021-02-19 00:44:01 +0300

    Добавьте файлы проекта.

---
+++ TaskForCleverence/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TaskForCleverence
+{
+    public static class MeServer
+    {
+        private static int count;
+        private static ReaderWriterLock locker = new ReaderWriterLock();
+
+        public static void AddToCount(int value)
+        {
+            locker.AcquireReaderLock(Timeout.InfiniteTimeSpan);
+            count += value;
+            locker.ReleaseWriterLock();
+        }
+
+        public static int GetCount()
+        {
+            locker.AcquireReaderLock(Timeout.InfiniteTimeSpan);
+            try
+            {
+                return count;
+            }
+            finally
+            {
+                locker.ReleaseReaderLock();
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+
+            Console.ReadLine();
+        }
+
+    }
+}
